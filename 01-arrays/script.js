d5f276f13dc60d246e7ce40544129dcccab64b42
arielmusa <arielmusainfo@gmail.com>
2025-04-03 15:40:43 +0200

    01-arrays/script.js: 5th excercize

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -18,7 +18,8 @@ const firstTeacher = teachers.shift();
 console.log(firstTeacher);
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
-
+teachers.unshift("Vanessa");
+console.table(teachers);
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
 
 // 7. Trova l'indice dell'insegnante 'Lewis' nell'array teachers
